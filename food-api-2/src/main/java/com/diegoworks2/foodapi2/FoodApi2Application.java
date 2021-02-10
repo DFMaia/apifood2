b647981c7e7deb3ffff17af4ec4fd27d14aff654
Diego Maia <diego.maia@montreal.com.br>
2021-02-10 09:44:51 -0300

    Adição de dependências starter web, starter data jpa, devtools, configuration processor, mysql connector. Além de configurar as propriedades de conexão com DB e criação do mesmo em application properties.

---
+++ food-api-2/src/main/java/com/diegoworks2/foodapi2/FoodApi2Application.java
@@ -0,0 +1,13 @@
+package com.diegoworks2.foodapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApi2Application.class, args);
+	}
+
+}
---
+++ food-api-2/src/test/java/com/diegoworks2/foodapi2/FoodApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoworks2.foodapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
