4c72be00856182a7218abe67ca448d10fc8a3fea
Diego Maia <diego.maia@montreal.com.br>
2021-02-11 11:52:18 -0300

    Adicionando uma interface repository e removendo classe de cozinha.

---
+++ food-api-2/src/main/java/com/diegoworks2/foodapi2/domain/repository/CozinhaRepository.java
@@ -0,0 +1,14 @@
+package com.diegoworks2.foodapi2.domain.repository;
+
+import java.util.List;
+
+import com.diegoworks2.foodapi2.domain.model.Cozinha;
+
+public interface CozinhaRepository {
+	
+	List<Cozinha> listar();
+	Cozinha buscar(Long id);
+	Cozinha salvar (Cozinha cozinha);
+	void remover(Cozinha cozinha);
+
+}
--- food-api-2/src/main/java/com/diegoworks2/foodapi2/infrastructure/repository/CozinhaRepositoryImpl.java
+++ food-api-2/src/main/java/com/diegoworks2/foodapi2/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -1,4 +1,4 @@
-package com.diegoworks2.foodapi2.jpa;
+package com.diegoworks2.foodapi2.infrastructure.repository;
 
 import java.util.List;
 
@@ -6,30 +6,32 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.springframework.stereotype.Component;
-
 import com.diegoworks2.foodapi2.domain.model.Cozinha;
+import com.diegoworks2.foodapi2.domain.repository.CozinhaRepository;
 
-@Component
-public class CadastroCozinha {
-	
+public class CozinhaRepositoryImpl implements CozinhaRepository{
+
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@Override
 	public List<Cozinha> listar(){
 		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
 	
+	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
 	@Transactional
+	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
 	
 	@Transactional
+	@Override
 	public void remover(Cozinha cozinha) {
 		cozinha = buscar(cozinha.getId());
 		manager.remove(cozinha);
