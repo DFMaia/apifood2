3a430a211b251c207b59a5b3744beab43e4316fa
Diego Maia <diego.maia@montreal.com.br>
2021-02-11 13:44:16 -0300

    Notação @Lombok foi usada e inserção de comentário para explicar o approach disso

--- food-api-2/src/main/java/com/diegoworks2/foodapi2/domain/model/Cozinha.java
+++ food-api-2/src/main/java/com/diegoworks2/foodapi2/domain/model/Cozinha.java
@@ -1,3 +1,11 @@
+/**
+ *Use o lombok para reduzir códigos boiler plate nas classes.
+OBS: Ao usar o @Data (que inclui um get, set, equals, hash e toString) você pode querer definir o 
+equals e o hash para um atributo em específico. Nessa cado coloque na classe a notação abaixo:
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+E acima do atributo ID coloque: @EqualsAndHashCode.Include 
+ */
+
 package com.diegoworks2.foodapi2.domain.model;
 
 import javax.persistence.Entity;
@@ -5,54 +13,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 	
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 }
